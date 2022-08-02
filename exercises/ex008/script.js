e8ea96e229d9e2e2d6b3d2a0425e397f13278d6b
Benito Miyazato <benito.miya172@gmail.com>
2022-08-02 16:24:28 -0300

    ex008

---
+++ exercises/ex008/script.js
@@ -0,0 +1,13 @@
+let username = "Dev Dev Dev AAA";
+let phoneNumber = "99013-2597";
+
+console.log(username.length);
+console.log(username.charAt(8));
+console.log(username.indexOf("v"));
+console.log(username.lastIndexOf("v"));
+console.log(username.trim());
+console.log(username.toUpperCase());
+console.log(username.toLowerCase());
+
+phoneNumber = phoneNumber.replaceAll("-", "");
+console.log(phoneNumber);
