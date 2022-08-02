4bc97414436857023079747443ccff6d1fd9f8dd
Benito Miyazato <benito.miya172@gmail.com>
2022-08-02 14:33:31 -0300

    ex004

--- exercises/ex004/script.js
+++ exercises/ex004/script.js
@@ -1,1 +1,6 @@
 // const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius = Number(window.prompt("Enter the radius of your circle"));
+let circunference = radius * 2 * PI;
+console.log("The circunference of this circle is: ", circunference);
