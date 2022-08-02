83fc3aaa5a689b947eaaca683f89fa93073af04c
Benito Miyazato <benito.miya172@gmail.com>
2022-08-02 15:00:32 -0300

    style

--- exercises/ex006/script.js
+++ exercises/ex006/script.js
@@ -5,7 +5,7 @@ document.getElementById("submit-button").onclick = function () {
   let b = document.getElementById("b-input").value;
   let hypotenuse = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
   body.innerHTML +=
-    "<p>" +
+    "<p style='text-align: center; font-weight: bold;background-color: tomato; color: white; padding: .5rem; width: 13rem;'>" +
     "The hypotenuse of this triangle is equal to: " +
     hypotenuse +
     "</p>";
