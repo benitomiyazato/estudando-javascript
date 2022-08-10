2605c5723d944cbe51b0181540284e28d77864b7
Benito Miyazato <benito.miya172@gmail.com>
2022-08-10 14:33:30 -0300

    async functions

--- exercises/ex032/asyncFunction.js
+++ exercises/ex032/asyncFunction.js
@@ -10,3 +10,12 @@ async function loadFile() {
 loadFile()
   .then((value) => console.log(value))
   .catch((error) => console.log(error));
+
+async function startProcess() {
+  try {
+    let message = await loadFile();
+    console.log(message);
+  } catch (error) {
+    console.log(error);
+  }
+}
