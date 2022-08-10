a2de140f55f5e188b797b67b87cab55ef06071f6
Benito Miyazato <benito.miya172@gmail.com>
2022-08-10 17:19:27 -0300

    mini game with event listeners

---
+++ exercises/ex035/addEventListener.js
@@ -0,0 +1,24 @@
+let x = 0;
+let y = 0;
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      y -= 5;
+      player.style.top = y + "px";
+      break;
+    case "ArrowDown":
+      y += 5;
+      player.style.top = y + "px";
+      break;
+    case "ArrowRight":
+      x += 5;
+      player.style.left = x + "px";
+      break;
+    case "ArrowLeft":
+      x -= 5;
+      player.style.left = x + "px";
+      break;
+  }
+});
+
+const player = document.querySelector(".player");
